f049d4cb25dad8f55836a3a9d655511fd34815fe
Trevor Kimenye <kimenye@gmail.com>
2015-02-03 19:16:52 +0300

    fixed error in sidekiq

--- app/workers/whatsapp_worker.rb
+++ app/workers/whatsapp_worker.rb
@@ -8,7 +8,7 @@ class WhatsappWorker
     url = Rails.application.secrets.ongair_url 
     token = Rails.application.secrets.ongair_token 
 
-    response = HTTParty.post(url, body: { token: token, phone_number: phone_number, text: text, thread: true }, debug_output: $stdout)
+    response = HTTParty.post(url, body: { token: token, phone_number: phone_number, text: message, thread: true }, debug_output: $stdout)
   end
 
 end
