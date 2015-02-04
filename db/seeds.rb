d75b716a8ada60b3d2193441e144527721ef0b55
muaad <muadh24@gmail.com>
2015-02-04 14:12:54 +0300

    Fetch BBC articles through juicer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 
 
-current_affairs_sources = [ "KenyaBroadcastingCorporation", "NewsWeb"].to_json
+current_affairs_sources = { sources: [ "KenyaBroadcastingCorporation", "NewsWeb"], sections: ""}.to_json
 keywords = ["CORD", "Jubilee", "Raila", "Uhuru", "TSC", "Al-shabaab"].to_json
 current_affairs = Category.create! name: "Current Affairs", source: "Juicer", description: "Politics, current affairs", keywords: keywords, metadata: current_affairs_sources
+
+sport_sources = { sources: ["NewsWeb"], sections: "BBC News - Africa, BBC Sport - Football"}.to_json
+keywords = ["Africa cup of nations", "English Premier League", "la liga", "Serie A", "Kenya Premier League", "FIFA"].to_json
+sport = Category.create! name: "Sport", source: "Juicer", description: "Sports News", keywords: keywords, metadata: sport_sources
+
+tech_sources = { sources: ["NewsWeb"], sections: "Technology"}.to_json
+keywords = [""].to_json
+tech = Category.create! name: "Technology", source: "Juicer", description: "Technology News", metadata: tech_sources, keywords: keywords
--- lib/bbc.rb
+++ lib/bbc.rb
@@ -13,17 +13,18 @@ class BBC
 		Juicer.new(Rails.application.secrets.juicer_api_key)
 	end
 
-	def articles text="", product, published_after, sections="", category
-		juicer.articles({text: text, product: product, published_after: published_after, sections: sections}) # "BBC News - Africa, BBC Sport - Football"
+	def create_articles text="", sections="", product, published_after, category
+		articles = juicer.articles({text: text, product: product, published_after: published_after, sections: sections, recent_first: true}) # "BBC News - Africa, BBC Sport - Football"
 		if !articles.empty?
       articles.each do |article|            
         is_new = Article.find_by(external_id: article["cps_id"], source: category.source).nil?
 
         if is_new
           metadata = { published: article["published"], product_source: article["source"] }.to_json
-          Article.create! category: category, source: "Juicer", title: article["title"], external_id: article["cps_id"],
+          Article.create! category: category, source: "Juicer", title: article["title"], external_id: article["cps_id"], article_type: "Web",
             url: article["url"], image_url: article["image"]["src"], summary: article["description"], metadata: metadata
         end
       end
+    end
 	end
 end
