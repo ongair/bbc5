346ae31598568ff76601d8563286c95938159d69
Trevor Kimenye <kimenye@gmail.com>
2015-02-04 08:31:25 +0300

    Added wechat api registration

--- app/controllers/api/v1/subscription_controller.rb
+++ app/controllers/api/v1/subscription_controller.rb
@@ -5,23 +5,23 @@ module Api::V1
 
     skip_before_action :verify_authenticity_token
 
-    # GET /version 
+    # GET /subscription/version 
     def version
       render json: { version: "1.0" }
     end
 
-    # GET /subscribers
+    # GET /subscription/subscribers
     def subscribers
       render json: { subcribers: Subscriber.all }
     end  
 
-    # POST /subscribers/sms
+    # POST /subscription/sms
     def sms_subscribers
       # create a subscriber from an SMS source
       puts params
     end
 
-    # POST /subscribers/whatsapp
+    # POST /subscription/whatsapp
     def whatsapp_subscribers
       # Whatsapp messages from Ongair
       if params[:text].downcase == Rails.application.secrets.subscribe_keyword.downcase && Subscriber.find_by(external_id: params[:phone_number], source: "WhatsApp").nil?
@@ -37,9 +37,18 @@ module Api::V1
       render json: { success: true }
     end
 
-    # POST /subscribers/wechat
+    # POST /subscription/wechat
     def wechat_subscribers
       # WeChat messages
+    end
+
+    # GET /subscription/wechat
+    def wechat_api_registration
+      if params.has_key?(:echostr)
+        render text: params[:echostr]
+      else
+        render json: { error: true }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
       post "subscription/sms" => "subscription#sms_subscribers"
       post "subscription/whatsapp" => "subscription#whatsapp_subscribers"
+      get "subscription/wechat" => "subscription#wechat_api_registration"
     end
   end
 
